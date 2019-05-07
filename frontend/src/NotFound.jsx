3c0e16387bf289048549bd412e7a02ed212f6c99
Tarasgroupki <kryvtsun16061996@gmail.com>
2019-05-07 16:11:52 +0300

    Added Swagger api annotation and page 404.

---
+++ frontend/src/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './NotFound.css';
+import { Link } from 'react-router-dom';
+//import PageNotFound from '../assets/images/PageNotFound';
+const NotFound = () => (
+   <body className="not-found">
+    < div className='c'>
+    < div className='_404'> 404 </div>
+<hr />
+    <div className='_1'>THE PAGE</div>
+    <div className='_2'>WAS NOT FOUND</div>
+    <a className='btn_back' href='/'>BACK TO MARS</a>
+</div>
+   </body>
+);
+export default NotFound;
